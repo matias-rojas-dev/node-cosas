ebb5715078c8d256c6d9979018c1367d7af5e00b
matias-rojas-dev <maigrojas.dev@gmail.comgit config --global user.email maigrojas.dev@gmail.com>
2025-02-23 19:45:49 -0300

    Inicio proyecto con TS

---
+++ 03-typescript/src/app.js
@@ -0,0 +1,25 @@
+const heroes = [
+  {
+    id: 1,
+    name: "Ironman",
+    owner: "Marvel",
+  },
+  {
+    id: 2,
+    name: "Spiderman",
+    owner: "Marvel",
+  },
+  {
+    id: 3,
+    name: "Batman",
+    owner: "DC",
+  },
+]
+
+const findHeroById = (id) => {
+  return heroes.find((heroe) => heroe.id === id)
+}
+
+const hero = findHeroById(3)
+
+console.log(hero?.name ?? "Hero not found")
