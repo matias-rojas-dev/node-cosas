4e90d8325144d310c98cb17ec73261437077dab2
matias-rojas-dev <maigrojas.dev@gmail.comgit config --global user.email maigrojas.dev@gmail.com>
2025-03-02 22:16:58 -0300

    Usando Yargs empiricamente

--- 04-multiplication/src/app.logic.ts
+++ 04-multiplication/src/app.logic.ts
@@ -1,18 +1,22 @@
 import fs from "fs"
+import { yarg } from "./config/plugins/yargs.plugin"
+
+const { b: base, l: limit, s: show } = yarg
 
 let outputMessage = ""
-const base = 5
 const headerMessage = `
 ================================
       Tabla del ${base}
 ================================
 `
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= limit; i++) {
   outputMessage += `${base} x ${i} = ${base * i} \n`
 }
 
 outputMessage = headerMessage + outputMessage
+
+if (show) console.log(outputMessage)
 
 const outputPath = `outputs`
 
--- 04-multiplication/src/config/plugins/yargs.plugin.ts
+++ 04-multiplication/src/config/plugins/yargs.plugin.ts
@@ -1,4 +1,4 @@
-import yargs from "yargs"
+import yargs, { check, options } from "yargs"
 import { hideBin } from "yargs/helpers"
 
 export const yarg = yargs(hideBin(process.argv))
@@ -19,5 +19,9 @@ export const yarg = yargs(hideBin(process.argv))
     type: "boolean",
     default: false,
     describe: "Show multiplication table",
+  })
+  .check((argv, options) => {
+    if (argv.b < 1) throw "Error: Base must be greater than 0"
+    return true
   })
   .parseSync()
