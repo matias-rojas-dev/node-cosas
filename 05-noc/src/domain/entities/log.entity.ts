14680cfe1f5fbc9e161140244c752cd90ae3ad91
matias-rojas-dev <maigrojas.dev@gmail.com>
2025-04-13 20:59:24 -0400

    LogEntity FromJson - Depuración

--- 05-noc/src/domain/entities/log.entity.ts
+++ 05-noc/src/domain/entities/log.entity.ts
@@ -30,6 +30,7 @@ export class LogEntity {
   }
 
   static fromJson = (json: string): LogEntity => {
+    // json = json === '' ? '{}' : json
     const { message, level, createdAt, origin } = JSON.parse(json)
     if (!message) throw new Error('Message is required')
     if (!level) throw new Error('Level is required')
